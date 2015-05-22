dd159aa6d3bb9d80171dc94f0e5cbafd88ec161b
Hannah Bown <hbown1@swarthmore.edu>
2015-05-22 14:59:39 -0400

    Chart shows up (in background?)

--- public/chart_canvas.js
+++ public/chart_canvas.js
@@ -11,8 +11,30 @@ function drawChart(json_data){
 
 	//appending canvas
 	$('#container').append('<canvas id="myChart" width="400" height="400"></canvas>');
-
-	
+	var data = [
+    	{
+        	value: 300,
+        	color:"#F7464A",
+        	highlight: "#FF5A5E",
+        	label: "Red"
+    	},
+    	{
+        	value: 50,
+        	color: "#46BFBD",
+        	highlight: "#5AD3D1",
+        	label: "Green"
+    	},
+    	{
+        	value: 100,
+        	color: "#FDB45C",
+        	highlight: "#FFC870",
+        	label: "Yellow"
+    	}
+	]
+	// Get context with jQuery - using jQuery's .get() method.
+	var ctx = $("#myChart").get(0).getContext("2d");
+	// This will get the first returned node in the jQuery collection.
+	var myNewChart = new Chart(ctx).Doughnut(data, {animateScale : false});
 
 
 
