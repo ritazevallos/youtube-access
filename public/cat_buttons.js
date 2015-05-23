a0df61b983a9d987cb0cab7abf6fbb0df7c7802a
Hannah Bown <hbown1@swarthmore.edu>
2015-05-23 14:56:34 -0400

    All buttons are trailers.

--- public/cat_buttons.js
+++ public/cat_buttons.js
@@ -4,19 +4,22 @@ function addButtons(cat_counts){
 	for(i=0; i<cat_counts.length; i++){
 
 		var cat = cat_counts[i];
+		cat_name = cat['title'];
 
-    	//Create an input type dynamically.   
-    	var element = document.createElement("input");
-    	//Assign different attributes to the element. 
-    	element.type = cat;
-    	element.value = cat; // Really? You want the default value to be the type string?
-    	element.name = cat.name;  // And the name too?
-    	element.onclick = function() { // Note this is a function
-        	alert("blabla");
-    	};
+		console.log(cat_name);
 
-    	//var foo = document.getElementById("fooBar");
-    	//Append the element in page (in span).  
-    	//foo.appendChild(element);
+		if(cat['num_captioned'] == 0 && cat['num_not_captioned'] == 0){
+			continue;
+		}
+
+    	var $button = $('<button></button>')
+    		.addClass('button')
+    		.attr('id', 'buttonid')
+    		.attr('onclick', 'drawChart(cat_counts, cat_name)')
+    	$button
+    		.append(cat_name);
+
+    	$('#view_as').append($button);
+
 	}
 }
