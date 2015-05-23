b8514c61e4afddd45d85cfec5c8a18beda6dfa68
Rita Zevallos <ritazevallos@Rita-Zevalloss-MacBook-Pro.local>
2015-05-22 20:26:16 -0400

    switched back

---
+++ public/backend_helper.js
@@ -0,0 +1,138 @@
+/* important in case there are multiple dates in the results */
+
+function convertDBresultsToCatCounts(results){
+  
+  console.log('database query results:');
+
+  var cat_counts = [];
+  var cc_indices = {}; // initialize a map between cat names and indices in the cat_counts array
+  
+  // initialize cat_counts
+  for (var i=0; i<Object.keys(clean_cats).length; i++){
+    var title = Object.keys(clean_cats)[i];
+    cc_indices[title] = i;
+    cat_counts.push({});
+    cat_counts[i]['title'] = title;
+    cat_counts[i]['id'] = clean_cats[title];
+    cat_counts[i]['num_captioned'] = 0;
+    cat_counts[i]['num_not_captioned'] = 0;
+  }
+
+  for (var i=0; i<results.length; i++){
+    var result = results[i].toJSON();
+    console.log(result);
+    var index = cc_indices[result['cat_name']];
+    cat_counts[index]['num_captioned'] += result['num_captioned'];
+    cat_counts[index]['num_not_captioned'] += result['num_not_captioned'];
+  }
+
+  return cat_counts;
+}
+
+
+
+
+function callAPIforDate(date, next_date){
+
+  cat_counts = [];
+  
+  // making this a dictionary so it's passed by reference
+  num_completed = { 'completed' : 0, 'total' : 2*Object.keys(clean_cats).length };
+
+  var promises = [];
+
+  for (var title in clean_cats){
+    cat_counts.push({});
+    var cat_id = clean_cats[title];
+    cat_counts[i]['id'] = cat_id;
+    cat_counts[i]['title'] = title;
+    cat_counts[i]['num_captioned'] = 0;
+    cat_counts[i]['num_not_captioned'] = 0;
+
+    promises.push(singleAPICall(date, next_date, cat_id, true, cat_counts, i, num_completed));
+    promises.push(singleAPICall(date, next_date, cat_id, false, cat_counts, i, num_completed));
+
+  }
+
+  Promise.all(promises).then(function(dataArr){
+    dataArr.forEach(function(data){
+      cat_counts[data['i']][data['capt_key']] += count;
+    });
+
+    return cat_counts;
+  }).catch(function(err){
+    console.log(err);
+  })
+
+}
+
+
+/* cat_counts is passed by reference */
+function singleAPICall(date, next_date, cat_id, captioned, cat_counts, i, num_completed){
+
+  var capt_str, capt_key;
+  if (captioned){
+    capt_str = "closedcaption";
+    capt_key = "num_captioned";
+  } else {
+    capt_str = "none";
+    capt_key = "num_not_captioned";
+  }
+
+  var api_url = "https://www.googleapis.com/youtube/v3/search?publishedAfter="+
+    date.toJSON() + "&publishedBefore="+next_date.toJSON() +
+    "&order=date&part=id&videoCaption="+capt_str+
+    "&type=video&videoCategoryId="+cat_id+
+    "&maxResults=0&key=AIzaSyA2qExhE65k0s4SCHl2wwcCWyPdgtoTyFg";
+
+  var promise = $.ajax( {
+    url: api_url,
+    index: i,
+    date: date,
+    type: 'GET'
+  } );
+
+  promise.done( function( response) {
+      addData(response,this.index, this.date);
+
+      function addData(data, i, date){
+        var count = parseInt(data.pageInfo.totalResults);
+        console.log(count);
+
+        if (count >= 1000000){
+
+          console.log('Count for the datetime range '+date+' - '+next_date + 'for category ' +cat_id +' exceeded the Youtube API max return of 1 000 000. Retrying by dividing date range in half.');
+          var diff = Math.abs(next_date - date);
+          var half_date = new Date();
+          half_date.setTime(date.getTime() + diff/2);
+          num_completed['total'] += 1 // since we're adding 2 more calls
+
+          var promise1 = singleAPICall(date, half_date, cat_id, captioned, cat_counts, i, num_completed);
+          var promise2 = singleAPICall(half_date, next_date, cat_id, captioned, cat_counts, i, num_completed);
+
+          $.when(promise1, promise2).done(function(data1, data2){
+            return {
+              'i' : i,
+              'count' : data1['count'] + data2['count'],
+              'capt_key' : data1['capt_key'] 
+            }
+          })
+
+        } else {
+          return {
+            'i' : i,
+            'count' : count,
+            'capt_key' : capt_key
+          }
+        
+        }
+      }
+    }
+  )
+
+  promise.fail( function (response){
+      throw 'Error accessing API: '+response;
+    }
+  );
+
+}
